f8303c50a5bc67bd3c25b295e7f8cbc358d46760
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2021-08-02 15:23:28 +0300

    Update RecipesController.cs

--- WhoCooks/Controllers/RecipesController.cs
+++ WhoCooks/Controllers/RecipesController.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace WhoCooks.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
@@ -7,18 +7,38 @@ namespace WhoCooks.Controllers
     using WhoCooks.Data;
     using WhoCooks.Models;
     using WhoCooks.Models.Recipes;
-
+    using WhoCooks.Services.Recipes;
 
     public class RecipesController :Controller
     {
 
         private readonly WhoCooksDbContext data;
+        private readonly IRecipeService recipe; 
 
-        public RecipesController(WhoCooksDbContext data)
+        public RecipesController(WhoCooksDbContext data, IRecipeService recipe)
         {
+            this.recipe = recipe;    
             this.data = data;
         }
+        public IActionResult All([FromQuery] AllRecipesQueryModel query)
+        {
+            var queryResult = this.recipe.All(
+                query.Title,
+                query.SearchTerm,
+                query.Sorting,
+                query.CurrentPage,
+                AllRecipesQueryModel.RecipesPerPage);
 
+            var recipesTitles = this.recipe.AllRecipes();
+
+            query.Titles = recipesTitles;
+            query.TotalRecipes = queryResult.TotalRecipes;
+            query.Recipes = queryResult.Recipes;
+
+            return View(query);
+        }
+
+    
         public IActionResult Add()
         {
             return View(new AddRecipeFormModel()
@@ -26,6 +46,45 @@ namespace WhoCooks.Controllers
                 Categories = this.GetRecipesCategories(),
                 Methods = this.GetRecipesMethods()
             });
+        }
+        [HttpPost]
+        public IActionResult Add(AddRecipeFormModel recipe)
+        {
+            
+
+            if (!this.data.Categories.Any(c => c.Id == recipe.CategoryId))
+            {
+                this.ModelState.AddModelError(nameof(recipe.CategoryId), "Category does not exist.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                recipe.Categories = this.GetRecipesCategories();
+
+                return View(recipe);
+            }
+
+            var recipeData = new Recipe
+            {
+                Title= recipe.Title,
+                Author= recipe.Author,
+                CookingMethodId=recipe.CookingMethodId,
+                Difficulty=recipe.Difficulty,
+                Servings=recipe.Servings,
+                CookTime=recipe.CookTime,
+                Ingredients=recipe.Ingredients,
+                TimeStamp= recipe.TimeStamp,
+                ImageUrl=recipe.ImageUrl,
+                Directions=recipe.Directions,
+                CategoryId=recipe.CategoryId
+
+
+            };
+
+            this.data.Recipes.Add(recipeData);
+            this.data.SaveChanges();
+
+            return RedirectToAction(nameof(All));
         }
         private IEnumerable<CategoryViewModel> GetRecipesCategories()
             => this.data
