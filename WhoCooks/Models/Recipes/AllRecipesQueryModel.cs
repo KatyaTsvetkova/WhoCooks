ec7cde580437b40eeb6a2d4456d4a373398b1281
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2021-08-02 15:27:14 +0300

    Add files via upload

---
+++ WhoCooks/Models/Recipes/AllRecipesQueryModel.cs
@@ -0,0 +1,26 @@
+﻿namespace WhoCooks.Models.Recipes
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using WhoCooks.Services.Recipes;
+
+    public class AllRecipesQueryModel
+    {
+
+        public const int RecipesPerPage = 5;
+        public string Title { get; init; }
+
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; init; }
+
+        public RecipeSorting Sorting { get; init; }
+
+        public int CurrentPage { get; init; } = 1;
+
+        public int TotalRecipes { get; set; }
+        public IEnumerable<string> Titles { get; set; }
+
+        public IEnumerable<RecipeServiceModel> Recipes { get; set; }
+
+    }
+}
