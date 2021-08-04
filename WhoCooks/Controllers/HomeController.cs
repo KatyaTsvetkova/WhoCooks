09f5a07f7aed3191cf3850eafdf69e2d785e98bb
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2021-08-04 13:56:25 +0300

    Update HomeController.cs

--- WhoCooks/Controllers/HomeController.cs
+++ WhoCooks/Controllers/HomeController.cs
@@ -1,26 +1,46 @@
-﻿namespace WhoCooks.Controllers
+namespace WhoCooks.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Caching.Memory;
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using WhoCooks.Models;
+    using System.Collections.Generic; 
+    using System.Linq; 
+    using WhoCooks.Services.Recipes;
+
     public class HomeController : Controller
     {
-         
+        private readonly IRecipeService recipes;
+        private readonly IMemoryCache cache;
+
+        public HomeController(
+            IRecipeService recipes,
+            IMemoryCache cache)
+        {
+            this.recipes = recipes;
+            this.cache = cache;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
- 
+            const string latestRecipesCacheKey = "LatestCarsCacheKey";
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var latestRecipe = this.cache.Get<List<LastestRecipeServiceModel>>(latestRecipesCacheKey);
+
+            if (latestRecipe == null)
+            {
+                //latestRecipe = this.recipes
+                   //.Latest()
+                   //.ToList();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(15));
+
+                this.cache.Set(latestRecipesCacheKey, latestRecipe, cacheOptions);
+            }
+
+            return View(latestRecipe);
         }
+
+        public IActionResult Error() => View();
     }
 }
